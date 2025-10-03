63ebfd7a283b221c01825a15b582aa659bccc61d
xShandora <estefaniaventusa@gmail.com>
2025-10-03 11:09:33 +0200

    second_comit

--- templates/views/navegacion.part.php
+++ templates/views/navegacion.part.php
@@ -1,4 +1,17 @@
 <!-- Navigation Bar -->
+<?php 
+  function esOpcionMenuActiva ( $opcion ) { 
+    $actual = explode('/',$_SERVER['REQUEST_URI']); 
+    $actual = '/'.$actual[count($actual)-1]; 
+    if ( $actual === $opcion) { 
+      return true; 
+    } 
+    else { 
+      return false; 
+    } 
+  } 
+?> 
+
    <nav class="navbar navbar-fixed-top navbar-default">
      <div class="container">
          <div class="navbar-header">
@@ -14,10 +27,48 @@
          </div>
          <div class="collapse navbar-collapse navbar-right" id="menu">
             <ul class="nav navbar-nav">
-              <li class="active lien"><a href="#"><i class="fa fa-home sr-icons"></i> Home</a></li>
-              <li class=" lien"><a href="about.php"><i class="fa fa-bookmark sr-icons"></i> About</a></li>
-              <li class=" lien"><a href="blog.php"><i class="fa fa-file-text sr-icons"></i> Blog</a></li>
-              <li><a href="contact.php"><i class="fa fa-phone-square sr-icons"></i> Contact</a></li>
+              <?php 
+                if (esOpcionMenuActiva('/index.php')==true || esOpcionMenuActiva('/')==true) 
+                  echo '<li class="active lien">';
+                else 
+                  echo '<li class="lien">';
+              ?> 
+              <a href="index.php">Home</a></li>
+
+              <?php 
+                if (esOpcionMenuActiva('/home.php')==true) 
+                  echo '<li class="active lien">';
+                else 
+                  echo '<li class="lien">';
+              ?>
+              <a href="#"><i class="fa fa-home sr-icons"></i> Home</a></li>
+
+
+              <?php 
+                if (esOpcionMenuActiva('/about.php')==true) 
+                  echo '<li class="active lien">';
+                else 
+                  echo '<li class="lien">';
+              ?>
+              <a href="about.php"><i class="fa fa-bookmark sr-icons"></i> About</a></li>
+
+
+              <?php 
+                if (esOpcionMenuActiva('/blog.php')==true) 
+                  echo '<li class="active lien">';
+                else 
+                  echo '<li class="lien">';
+              ?>
+              <a href="blog.php"><i class="fa fa-file-text sr-icons"></i> Blog</a></li>
+
+
+              <?php 
+                if (esOpcionMenuActiva('/contact.php')==true) 
+                  echo '<li class="active lien">';
+                else 
+                  echo '<li class="lien">';
+              ?> 
+              <a href="contact.php"><i class="fa fa-phone-square sr-icons"></i> Contact</a></li>
             </ul>
          </div>
      </div>
